967e07d71daa6eabb3be00adebd602d77f875fa6
psyfer <psyferblock.io@gmail.com>
2022-06-19 16:09:17 +0300

    nav bar setup

--- src/App.js
+++ src/App.js
@@ -5,31 +5,29 @@ import NavBarAdmin from './components/NavBarAdmin';
 import NavBarUser from './components/NavBarUser';
 import Main from './components/Main';
 import AdminLogin from './components/AdminLogin';
-import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom"; 1
+import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom"; 
 // import { Link } from '@react-navigation/native';
 import ErrorPage from './components/ErrorPage';
 
 function App() {
   return (
     <>
-      yo
+    
       <Router>  
         <nav>
-          <Link>admin </Link>
-          <Link>user</Link>
-          <Link></Link>
-
-        </nav>
+       
       <Routes>
         <Route path="/" element={<NavBar/>}>
-          <Route path="admin-page" element={NavBarAdmin}/>
-          <Route path="user-page" element={NavBarUser}/>
-          <Route path="*" element={<ErrorPage />} />
+          <Route path="/admin-page" element={<NavBarAdmin/>}/>
+          <Route path="/user-page" element={<NavBarUser/>}/>
+          {/* <Route path="*" element={<ErrorPage />} /> */}
           {/* <Link to="#main-nav" >
             main nav
           </Link> */}
           </Route>
       </Routes>
+      </nav>
+
       </Router>
        
     </>
