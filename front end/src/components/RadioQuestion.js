24d3d6d7de49d7cf5d6cea2a35ef2ef51259b548
psyfer <psyferblock.io@gmail.com>
2022-06-20 21:10:45 +0300

    laptop giving issues/ async working to push questions

--- front end/src/components/RadioQuestion.js
+++ front end/src/components/RadioQuestion.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export default function RadioQuestion() {
 
-    const [question,setQuestion]=React.useState({})
-    const [answer1,setAnswer1]=React.useState({})
-    const [answer2,setAnswer2]=React.useState({})
-    const [answer3,setAnswer3]=React.useState({})
-    const [answer4,setAnswer4]=React.useState({})
+    const [question,setQuestion]=useState({})
+    const [answer1,setAnswer1]=useState({})
+    const [answer2,setAnswer2]=useState({})
+    const [answer3,setAnswer3]=useState({})
+    const [answer4,setAnswer4]=useState({})
+    
+     const getData=async()=>{
+        console.log(answer1,answer2,answer3,answer4)
+        const url="http://127.0.0.1:8000/api/add_question_radio";
+        const res= await fetch("http://127.0.0.1:8000/api/add_question_radio",{
+            method:'POST',
+            body:JSON.stringify({"question":question,"answer1":answer1,"answer2":answer2,"answer3":answer3,"answer4":answer4 }),
+            headers:{
+                "contret-type":"application/json",}
+        }) 
+            const data =await res.json()
+            console.log(data)
+
+        }
+    
 
   return (
     <div>
@@ -16,14 +31,14 @@ export default function RadioQuestion() {
         <div className="form-layout">
             <input onChange={e=>setQuestion(e.target.value)} type="text" className="form-question" placeholder="question "/>
 
-        <form>
-            <input onChange={e=>setAnswer1(e.target.value)} type="text" className="form-input" placeholder="option 1"/>
-            <input onChange={e=>setAnswer2(e.target.value)} type="text" className="form-input" placeholder="option 2"/>
-            <input onChange={e=>setAnswer3(e.target.value)} type="text" className="form-input" placeholder="option 3"/>
-            <input onChange={e=>setAnswer4(e.target.value)} type="text" className="form-input" placeholder="option 4"/>
+            
+                <input onChange={e=>setAnswer1(e.target.value)} type="text" className="form-input" placeholder="option 1"/>
+                <input onChange={e=>setAnswer2(e.target.value)} type="text" className="form-input" placeholder="option 2"/>
+                <input onChange={e=>setAnswer3(e.target.value)} type="text" className="form-input" placeholder="option 3"/>
+                <input onChange={e=>setAnswer4(e.target.value)} type="text" className="form-input" placeholder="option 4"/>
 
-            <button className="form-button" onClick={questionSubmit}>submit</button>
-        </form>
+                <button className="form-button" onClick={getData}>submit</button>
+            
         </div>
     </div>
   )
